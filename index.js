2c767a1ed76fe819daf074cddc5fbd38b12e276f
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2019-05-07 20:12:08 +0200

    create a function that saving the data to the firebase

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-
-// db.collection('users').get().then((snapshot) => {
-//     console.log(snapshot.docs)
-// })
-
 const list = document.querySelector('#list-of-name');
+const form = document.querySelector('#add-user-form');
 
 const getUserFromDb = (doc) => {
     let li = document.createElement('li');
@@ -25,9 +21,21 @@ const getUserFromDb = (doc) => {
     list.appendChild(li)
 }
 
+// getting data
 db.collection('users').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         console.log(doc.data().Name)
         getUserFromDb(doc)
     });
+})
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('users').add({
+        Name: form.name.value,
+        Surname: form.surname.value
+    })
+    form.name.value = '';
+    form.surname.value = '';
 })
